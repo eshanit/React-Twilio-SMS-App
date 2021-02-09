4decfccd11bbb4baa8ca8e5a9d97f5bd2d8aea0c
eshanit <tinashemangoro@gmail.com>
2021-02-09 01:52:43 -0500

    remove tailwing css dependancies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './src/**/*.{js,jsx,ts,tsx}',
     './public/index.html',
-    require('@tailwindcss/forms'),
+    //('@tailwindcss/forms'),
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
